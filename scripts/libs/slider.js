6899e0e5138a35aa05daaf981e1b8f42a69b103b
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-07-07 19:47:06 +0900

    insert pictures

--- scripts/libs/slider.js
+++ scripts/libs/slider.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   // direction: 'vertical',
   loop: true,
-
+  effect: "fade",
   // If we need pagination
   pagination: {
     // el: '.swiper-pagination',
